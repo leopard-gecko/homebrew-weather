66b0f943025940d9f909001958751ceb8faeddd2
leopardgecko <kaz_bashi@yahoo.co.jp>
2019-11-20 16:33:11 +0900

    改訂

--- weather.rb
+++ weather.rb
@@ -1,9 +1,9 @@
 class Weather <Formula
 
-  HOMEBREW_WEATHER_VERSION="1.0.3"
+  HOMEBREW_WEATHER_VERSION="1.0.4"
   
   url "https://github.com/leopard-gecko/homebrew-weather/releases/download/#{HOMEBREW_WEATHER_VERSION}/weather.zip"
-  sha256 "129be96eec552dd01efb95eb54bacea25bf30b904c6943ddfa7d8711257d72fa"
+  sha256 "0c4dc070f317f427d4c00621d4e56adc6d669024ca2b7004283a145802514cfc"
   version HOMEBREW_WEATHER_VERSION
   option "URL=", "表示させたい地域のURL"
 
