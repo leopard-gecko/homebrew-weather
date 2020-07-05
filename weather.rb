d22c569ca3d6f1ef364cc07957b94641cb20362a
leopard-gecko <23162102+leopard-gecko@users.noreply.github.com>
2020-07-05 22:26:50 +0900

    Update weather.rb

--- weather.rb
+++ weather.rb
@@ -5,14 +5,10 @@ class Weather <Formula
   url "https://github.com/leopard-gecko/homebrew-weather/releases/download/#{HOMEBREW_WEATHER_VERSION}/weather.zip"
   sha256 "b2c70e04247a32c3968c5c44782f0ef3d915f4bd754c8a6919172e140c7fba9a"
   version HOMEBREW_WEATHER_VERSION
-  option "URL=", "表示させたい地域のURL"
 
   def install
   
-    myurl = ARGV.value("URL")
-    if myurl == nil or myurl == "" then
-      myurl = "https://www.accuweather.com/en/jp/koto-ku/221230/weather-forecast/221230"
-    end
+     myurl = "https://www.accuweather.com/en/jp/koto-ku/221230/weather-forecast/221230"
     system("perl -pi -e 's*MY_WEATHER_URL*#{myurl}*g' w_*")
   
     bin.install Dir['*']
