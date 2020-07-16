86e9a2833ca0b6bd3a374bafa063971fbff0e36f
leopard-gecko <23162102+leopard-gecko@users.noreply.github.com>
2020-07-16 22:39:03 +0900

    改訂

--- weather.rb
+++ weather.rb
@@ -1,9 +1,9 @@
 class Weather <Formula
 
-  HOMEBREW_WEATHER_VERSION="1.0.8"
+  HOMEBREW_WEATHER_VERSION="1.0.9"
   
   url "https://github.com/leopard-gecko/homebrew-weather/releases/download/#{HOMEBREW_WEATHER_VERSION}/weather.zip"
-  sha256 "b2c70e04247a32c3968c5c44782f0ef3d915f4bd754c8a6919172e140c7fba9a"
+  sha256 "7da3ac2eed297b781530a55ebca4415956e16e2ef9b648fc234312c8bd660492"
   version HOMEBREW_WEATHER_VERSION
 
   def install
